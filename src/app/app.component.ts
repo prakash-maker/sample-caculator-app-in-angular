3da24ea47284cc8b899409827438d8acfa8e8120
prakash G <prakash41417@gmail.com>
2021-01-09 20:33:37 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,64 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-ec-app';
+  currentNumber = '0';
+  firstOperand = null;
+  operator = '';
+  waitForSecondNumber = false;
+
+  public getNumber(v: string) {
+    console.log(v);
+    if (this.waitForSecondNumber) {
+      this.currentNumber = v;
+      this.waitForSecondNumber = false;
+
+    } else {
+      this.currentNumber === '0' ? this.currentNumber = v : this.currentNumber += v;
+    }
+    
+  }
+  
+  getDecimal() {
+    if (!this.currentNumber.includes('.')) {
+      this.currentNumber += '.';
+    }
+  }
+  private doCalculation(op, secondOp) {
+    switch (op) {
+      case '+':
+       return this.firstOperand += secondOp;
+      case '-':
+        return this.firstOperand -= secondOp;
+      case '*':
+       return this.firstOperand *= secondOp;
+      case '/':
+       return this.firstOperand /= secondOp;
+      case '=':
+        return secondOp;
+    }
+  }
+  public getOperation(op: string){
+    console.log(op);
+
+    if(this.firstOperand === null){
+      this.firstOperand = Number(this.currentNumber);
+
+    }else if(this.operator){
+      const result = this.doCalculation(this.operator , Number(this.currentNumber))
+      this.currentNumber = String(result);
+      this.firstOperand = result;
+    }
+    this.operator = op;
+    this.waitForSecondNumber = true;
+
+    console.log(this.firstOperand);
+
+  }
+  public clear() {
+    this.currentNumber = '0';
+    this.waitForSecondNumber = false;
+    this.operator = null;
+    this.firstOperand = null;
+
+  }
 }
